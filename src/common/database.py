f4172aa8d3f156f323ce18e78867345803e21be5
theharisshah <shahharis383@gmail.com>
2018-01-05 12:57:15 +0530

    setting up mongo

--- src/common/database.py
+++ src/common/database.py
@@ -2,13 +2,13 @@ import pymongo
 
 
 class Database(object):
-    URI = "mongodb://localhost:27017"
+    URI = "mongodb://haris:haris123@ds239587.mlab.com:39587/heroku_vm734g3w"
     DATABASE = None
 
     @staticmethod
     def initalisation():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['assignment']
+        Database.DATABASE = client.get_default_database()
 
     @staticmethod
     def insert(collection, data):
